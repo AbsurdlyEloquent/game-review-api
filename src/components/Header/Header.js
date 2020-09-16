9677a9442ca8528cb88c64472fab163f1faf9ee3
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-16 14:47:25 -0400

    got rid of css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,14 @@ import React from 'react'
 class Header extends React.Component {
   render() {
     return (
-      <header></header>
+      <header>
+        <h1>Insert cool thing here here</h1>
+        <div className="user">
+          <p>Insert username</p>
+        </div>
+      </header>
     )
   }
 }
+
+export default Header
