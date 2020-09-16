7d358936eb2a7803153012d0c0f26ddc8e0d4350
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-16 15:22:25 -0400

    Updates

---
+++ precache-manifest.4d1f6c0274d0be176464f135070ea0ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3672c69df7f5e1947038058dc0edec3",
+    "url": "/game-review-api/index.html"
+  },
+  {
+    "revision": "f5b58d31a62df08fc80a",
+    "url": "/game-review-api/static/css/main.2d71a920.chunk.css"
+  },
+  {
+    "revision": "a82ae7433bec95b230d2",
+    "url": "/game-review-api/static/js/2.858969cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-review-api/static/js/2.858969cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5b58d31a62df08fc80a",
+    "url": "/game-review-api/static/js/main.ba066ad9.chunk.js"
+  },
+  {
+    "revision": "d4ee88efe93390c8265a",
+    "url": "/game-review-api/static/js/runtime-main.4e64b7a4.js"
+  }
+]);
