d5f179cdec349a079ba37e35110c6102d5f45e2b
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-16 15:17:16 -0400

    making responsive layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import './Header.css'
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <h1>Insert cool thing here here</h1>
+      <header className="header">
+        <h1 className="title">Insert cool thing here here</h1>
         <div className="user">
           <p>Insert username</p>
         </div>
