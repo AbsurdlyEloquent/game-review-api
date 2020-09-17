876b9c4a914edca7b4e8317cbc4c927484a3efb4
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-16 21:47:39 -0400

    fixing state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,23 @@ import React from 'react'
 import './Header.css'
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log(this.props.burgerHandler)
+    this.state = {
+      navActive: this.props.navActive,
+      burgerClass: this.props.burgerClass,
+      navClass: this.props.navClass
+    }
+  }
   render() {
     return (
       <header className="header">
+        <div id="hamburger" role="button" className={this.state.burgerClass} onClick={this.props.burgerHandler} >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
         <h1 className="title">Insert cool thing here here</h1>
         <div className="user">
           <p>Insert username</p>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,1 +1,24 @@
+import React from 'react'
+import './Nav.css'
 
+class Nav extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      navActive: this.props.navActive,
+      navClass: this.props.navClass
+    }
+  }
+  render() {
+    return(
+      <aside>
+        <nav className={`nav ${this.state.navClass}`}>
+
+        </nav>
+      </aside>
+    )
+  }
+}
+
+export default Nav
