6ea46f28c7144ea38398dd273e902c3d8f636b9b
Lex <55722511+LexLeach@users.noreply.github.com>
2020-09-17 19:40:27 -0400

    updated cards.js, cards.css added a button component for write a review

---
+++ src/components/Buttons/ReviewButton.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "../Buttons/Buttons.css"
+
+export default function ReviewButton() {
+    return (
+        <div className="rvw-btn">
+            <button className="review-btn">Write a Review</button>
+        </div>
+    )
+}
--- src/components/Cards/CardLight.js
+++ src/components/Cards/CardLight.js
@@ -6,11 +6,21 @@ import MMORPG from "../Tags/MMORPG"
 import PC from "../Tags/PC"
 import Adventure from "../Tags/Adventure"
 import Accordion from "../Accordion/Accordion"
+import ReviewButton from "../Buttons/ReviewButton"
 
 export default function CardLight() {
     return (
         <div className="card-light">
-            <div className="card-title"><h2>Final Fantasy VI</h2></div>
+            <div className="card-title">
+                <h2>Final Fantasy VI</h2>
+                <div className="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="far fa-star-half"></i>
+                </div>
+            </div>
             <div className="card-body">
                 <img className="card-img" src="https://images-na.ssl-images-amazon.com/images/I/51Ro%2BgfSjbL._AC_SX385_.jpg" alt="" />
                 <div className="card-body-ul">
@@ -19,6 +29,9 @@ export default function CardLight() {
                         <li className="li-tags">Genre:&nbsp;<MMORPG /></li>
                         <li className="li-tags">Tags:&nbsp;<Action />&nbsp;<Adventure /></li>
                     </ul>
+                </div>
+                <div className="btn">
+                    <ReviewButton />
                 </div>
             </div>
             <Accordion />
