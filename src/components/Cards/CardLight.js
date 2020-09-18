3decd67144d76bd5cc61aaf4a9d91950c6fb5148
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-17 22:15:35 -0400

    added card tags

--- src/components/Cards/CardLight.js
+++ src/components/Cards/CardLight.js
@@ -2,14 +2,14 @@ import React from "react"
 import "../Cards/Cards.css"
 import Accordion from "../Accordion/Accordion"
 import CardTitle from "../Card-Title/Card-Title"
-import CardBody from "../Card-Body/Card-Body"
+import CardBody from "../CardBody/CardBody"
 
 export default function CardLight(props) {
     return (
         <div className="card-light">
             <CardTitle>{props.game.name}</CardTitle>
-            <CardBody platform={props.game.platform} genre={props.game.genre} tags={props.game.tags}/>
-            <Accordion />
+            <CardBody platforms={props.game.platforms} genre={props.game.genre} tags={props.game.tags}/>
+            <Accordion reviews={props.reviews} />
         </div>
     )
 }
