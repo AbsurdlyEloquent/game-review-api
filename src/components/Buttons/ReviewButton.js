eb0b345c0192f714acc4025c39ad4eb863c6afd0
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-18 09:48:03 -0400

    turned review button into class

--- src/components/Buttons/ReviewButton.js
+++ src/components/Buttons/ReviewButton.js
@@ -2,11 +2,13 @@ import React from "react"
 import ReviewModal from '../Modal/ReviewModal'
 import "../Buttons/Buttons.css"
 
-export default function ReviewButton() {
+export default class ReviewButton extends React.Component {
+  render() {
     return (
-        <div className="rvw-btn">
-            <button className="review-btn">Write a Review</button>
-            <ReviewModal />
-        </div>
+      <div className="rvw-btn">
+        <button className="review-btn">Write a Review</button>
+          <ReviewModal />
+      </div>
     )
+  }
 }
