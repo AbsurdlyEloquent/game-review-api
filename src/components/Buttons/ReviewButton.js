08d18f5c3f5abe02a1a02bfb359939e7df9d97ea
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-18 10:39:51 -0400

    made input component

--- src/components/Buttons/ReviewButton.js
+++ src/components/Buttons/ReviewButton.js
@@ -15,7 +15,7 @@ export default class ReviewButton extends React.Component {
     return (
       <div className="rvw-btn">
         <button onClick={this.reviewHandler} className="review-btn">Write a Review</button>
-          <ReviewModal game={this.props.game} active={this.state.modalActive}/>
+          <ReviewModal game={this.props.game} active={this.state.modalActive} reviewHandler={this.reviewHandler}/>
       </div>
     )
   }
@@ -23,7 +23,7 @@ export default class ReviewButton extends React.Component {
     console.log(e.target)
     if (e.target.className === 'review-btn') {
       this.setState({ modalActive:'active' })
-    } else if (e.target.className === 'modal-wrapper') {
+    } else if (e.target.className === 'modal-wrapper active') {
       this.setState({ modalActive:'' })
     }
   }
--- src/components/Modal/ReviewModal.js
+++ src/components/Modal/ReviewModal.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import './Modal.css'
+import Input from '../Input/Input'
 
 class ReviewModal extends React.Component {
   render() {
     return (
-      <div className={`modal-wrapper ${this.props.active}`}>
-        <div className='modal'></div>
+      <div className={`modal-wrapper ${this.props.active}`} onClick={this.props.reviewHandler}>
+        <div className='modal'>
+          <div className='modal-header'></div>
+          <div className='modal-body'>
+            <form>
+              <Input type='overall' />
+              <Input type='difficulty' />
+              <Input type='gameplay' />
+              <Input type='graphics' />
+              <Input type='replayability' />
+            </form>
+          </div>
+          <div className='modal-footer'></div>
+        </div>
       </div>
     )
   }
