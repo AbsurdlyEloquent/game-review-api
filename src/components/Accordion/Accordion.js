e3d2add5a614eb31b5a2b582d4876f38e0c27569
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-17 21:25:34 -0400

    separated review into separate component

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Review from '../Review/Review'
 
 export default function Accordion() {
     return (
@@ -8,18 +9,7 @@ export default function Accordion() {
                 <section className="box">
                     <label className="box-title" htmlFor="cb1">Read More</label>
                     <label className="box-close" htmlFor="acc-close"></label>
-                    <div className="box-content">
-                        <div className="content-box">
-                            <h2 className="h2-title">Review By User</h2>
-                            <ul>
-                                <li>Overall: Epic!</li>
-                                <li>Difficulty: Epic!</li>
-                                <li>Graphics: It was the 2000s...</li>
-                                <li>Gameplay: Epic!</li>
-                                <li>Replayability: Hell yea!</li>
-                            </ul>
-                        </div>
-                    </div>
+                    <Review />
                 </section>
                 <input type="radio" name="accordion" id="acc-close" />
             </div>
---
+++ src/components/Review/Review.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Review() {
+  return (
+    <div className="box-content">
+        <div className="content-box">
+            <h2 className="h2-title">Review By User</h2>
+            <ul>
+                <li>Overall: Epic!</li>
+                <li>Difficulty: Epic!</li>
+                <li>Graphics: It was the 2000s...</li>
+                <li>Gameplay: Epic!</li>
+                <li>Replayability: Hell yea!</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
