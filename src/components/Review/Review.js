1614183c447be90d672e3fade9a345a384d7efb0
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-17 22:41:07 -0400

    filled in reviews

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-export default function Review() {
+export default function Review(props) {
   return (
     <div className="box-content">
         <div className="content-box">
             <h2 className="h2-title">Review By User</h2>
             <ul>
-                <li>Overall: Epic!</li>
-                <li>Difficulty: Epic!</li>
-                <li>Graphics: It was the 2000s...</li>
-                <li>Gameplay: Epic!</li>
-                <li>Replayability: Hell yea!</li>
+                <li>Overall: {props.overall}</li>
+                <li>Difficulty: {props.difficulty}</li>
+                <li>Graphics: {props.graphics}</li>
+                <li>Gameplay: {props.gameplay}</li>
+                <li>Replayability: {props.replayability}</li>
             </ul>
         </div>
     </div>
